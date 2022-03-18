81b9501dca7f3595ce0485ba52e2d9fd1c5c0496
Dong-Hoon <dhkim85@kw.ac.kr>
2022-03-18 11:57:30 +0900

    enhanced for

--- src/DivideAndConquer.java
+++ src/DivideAndConquer.java
@@ -1,25 +1,26 @@
+import java.util.Random;
 
 public class DivideAndConquer {
     private boolean isSomething(int start, int end) {
         return start >= end;
     }
 
-    public int[] mergeSort(int[] arr, int start, int end) {
-        if (isSomething(start, end)) {
-            return null;
-        }
+    // primitive type 원시 변수 [int, char, float, double, bool, short, long]
+    // instance variable
+    // 객체 [String, int[], Animal]: String str = new String("hello"); //
 
-        int mid = (start + end) / 2;
+    public void mergeSort(int[] arr, int start, int end) { // 6
+        if (isSomething(start, end)) { return; }   // 빠져나올 수 있는 조건
 
-        mergeSort(arr, 0, mid);
-        mergeSort(arr, mid+1, end);
+        int mid = (start + end) / 2;        // start = 0, end = 5, mid = 2
 
-        merge(arr, start, mid, end);
+        mergeSort(arr, 0, mid);         // 왼쪽 분할 [0-2]
+        mergeSort(arr, mid+1, end);     // 오른쪽 분할 [3-5]
 
-        return null;
+        merge(arr, start, mid, end);         // 왼쪽 오른쪽 병합
     }
 
-    private int[] merge(int[] arr, int start, int mid, int end) {
+    private void merge(int[] arr, int start, int mid, int end) {
         int p = start;
         int q = mid + 1;
         int idx = p;
@@ -34,26 +35,29 @@ public class DivideAndConquer {
             }
         }
 
-        for (int i=start; i<=end; i++) {
-            arr[i] = tmp[i];
-        }
-
-        return null;
+        if (end + 1 - start >= 0) System.arraycopy(tmp, start, arr, start, end + 1 - start);
     }
 
     public static void main(String[] args) {
-        int arr[] = {7, 6, 5, 4, 3, 2, 1, 0};
-        for (int i=0; i<arr.length; i++) {
-            System.out.printf("%d ", arr[i]);
+        Random r = new Random();
+
+        int[] arr = new int[20];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(1000);   // (0, 1000]
+        }
+
+        for (int k : arr) {
+            System.out.printf("%d ", k);
         }
         System.out.println();
 
         DivideAndConquer dac = new DivideAndConquer();
-        dac.mergeSort(arr, 0, arr.length-1);
+        dac.mergeSort(arr, 0, arr.length - 1);
 
-        for (int i=0; i<arr.length; i++) {
-            System.out.printf("%d ", arr[i]);
+        for (int j : arr) {
+            System.out.printf("%d ", j);
         }
+
         System.out.println();
     }
 }
